55e012c3a319012f1e1a4b1d08a543bac9ebbdd1
AFM-OPTICA <afm.optica@outlook.com>
2026-02-20 19:41:29 -0300

    sequencial image aquisition

---
+++ scope/teste2.py
@@ -0,0 +1,52 @@
+import cv2
+import pyvisa
+import time
+from io import BytesIO
+from PIL import Image
+import numpy as np
+
+RESOURCE_NAME = 'USB0::1689::1049::C030475::0::INSTR'
+rm = pyvisa.ResourceManager('@py')
+frames = 1000
+
+try:
+    scope = rm.open_resource(RESOURCE_NAME)
+    # Aumentamos o chunk_size para não truncar a leitura
+    scope.chunk_size = 1024 * 1024 # 1MB
+    scope.timeout = 1000
+
+    print("Configurando osciloscópio...")
+    scope.write('HARDCOPY:PORT USB')
+    scope.write('HARDCOPY:FORMAT BMP')
+    
+    for _ in range(frames):
+        print("Solicitando imagem...")
+        scope.write('HARDCOPY START')
+        time.sleep(0.5)
+
+        # Lendo os dados brutos
+        raw_data = scope.read_raw()
+        
+        # O manual explica que os dados vem como #<num_digits><length><data>
+        # Localizamos o 'BM' para ignorar o cabeçalho do protocolo
+        if b'BM' in raw_data:
+            offset = raw_data.find(b'BM')
+            image_bytes = raw_data[offset:]
+            
+            # # Salvando o BMP original
+            # with open("captura.bmp", "wb") as f:
+            #     f.write(image_bytes)
+                
+            # Convertendo para PNG automaticamente
+            img: np.typing.ArrayLike = Image.open(BytesIO(image_bytes))
+            img.save("resultado_final.png")
+
+            
+            print(f"Sucesso! Imagem convertida: resultado_final.png ({len(image_bytes)} bytes)")
+        else:
+            print("Dados incompletos recebidos.")
+
+except Exception as e:
+    print(f"Erro: {e}")
+finally:
+    if 'scope' in locals(): scope.close()
